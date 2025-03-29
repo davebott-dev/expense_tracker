0a4a9666c3cb560301a743b74af04a12fe01855e
= <bottenbergdave@gmail.com>
2025-03-29 07:51:49 -0400

    formatting the dashboard page

--- Client/src/routes/Dashboard.jsx
+++ Client/src/routes/Dashboard.jsx
@@ -1,10 +1,11 @@
 import {useState} from "react";
+import {Avatar} from "@mui/material"
 
 const Dashboard = () => {
   return (
     <>
       <div className = "welcome">
-        <div>Avatar</div>
+        <Avatar sx={{width:55, height:55}}>DB</Avatar>
         <div>
           <div>Welcome to your account!</div>
           <p>You are now logged in. You can use the widgets on the side to enter and track data.</p>
