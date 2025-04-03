05a21000a7f5df80752932975d881eaa938b6dcc
= <bottenbergdave@gmail.com>
2025-04-02 21:33:04 -0400

    stying the tracker

--- Client/src/routes/Dashboard.jsx
+++ Client/src/routes/Dashboard.jsx
@@ -1,25 +1,34 @@
-import {useState} from "react";
-import {Avatar} from "@mui/material"
+import { useState } from "react";
+import { Avatar } from "@mui/material";
 import Expense from "../components/Expense";
 import Transfer from "../components/Transfer";
 import Income from "../components/Income";
 
 const Dashboard = () => {
   const [transactionType, setTransactionType] = useState(0);
+  const [isActive, setIsActive] = useState("");
+
+  const handleTransactionTypeChange = (type) => {
+    setTransactionType(type);
+    setIsActive(type === 0 ? "expense" : type === 1 ? "transfer" : "income");
+  };
 
   return (
     <>
-      <div className = "welcome">
-        <Avatar sx={{width:55, height:55}}>DB</Avatar>
+      <div className="welcome">
+        <Avatar sx={{ width: 55, height: 55 }}>DB</Avatar>
         <div>
           <div>Welcome to your account!</div>
-          <p>You are now logged in. You can use the widgets on the side to enter and track data.</p>
+          <p>
+            You are now logged in. You can use the widgets on the side to enter
+            and track data.
+          </p>
           <button>Logout</button>
         </div>
       </div>
-      <div className = "widgets">
+      <div className="widgets">
         <div>
-          <div className= "section_header">
+          <div className="section_header">
             <h3>Net Worth </h3>
             <div>Net Worth</div>
           </div>
@@ -46,29 +55,54 @@ const Dashboard = () => {
         </div>
         <div>
           <div>
-            <div className= "section_header">
+            <div className="section_header">
               <h3>New Transaction </h3>
             </div>
             <div className="transaction_widget">
               <div className="transaction_type">
-                <div onClick = {()=> setTransactionType(0)}>Expense</div>
-                <div onClick = {()=> setTransactionType(1)}>Transfer</div>
-                <div onClick = {()=> setTransactionType(2)}>Income</div>
+                <div
+                  onClick={() => handleTransactionTypeChange(0)}
+                  className={isActive == "expense" ? "expense_transaction" : ""}
+                >
+                  Expense
+                </div>
+                <div
+                  onClick={() => handleTransactionTypeChange(1)}
+                  className={
+                    isActive == "transfer" ? "transfer_transaction" : ""
+                  }
+                >
+                  Transfer
+                </div>
+                <div
+                  onClick={() => handleTransactionTypeChange(2)}
+                  className={isActive == "income" ? "income_transaction" : ""}
+                >
+                  Income
+                </div>
               </div>
               {transactionType === 0 && <Expense />}
-              {transactionType === 1 && <Transfer> 
-                <p>Note: Make sure to select the correct accounts.</p>
-                </Transfer>}
-                {transactionType === 2 && <Income />}
+              {transactionType === 1 && (
+                <Transfer>
+                  <p>Note: Make sure to select the correct accounts.</p>
+                </Transfer>
+              )}
+              {transactionType === 2 && <Income />}
             </div>
-
           </div>
-          <div className= "section_header">
+          <div className="section_header">
             <h3>Recent Transaction </h3>
+          </div>
+          <div>
+            <div>
+              <p>Transaction 1</p>
+              <p>Amount</p>
+              <p>Date</p>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
-};  
+};
 export default Dashboard;
--- Client/src/routes/Root.jsx
+++ Client/src/routes/Root.jsx
@@ -1,51 +1,89 @@
 import { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
-import AutorenewIcon from '@mui/icons-material/Autorenew';
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import SyncAltIcon from "@mui/icons-material/SyncAlt";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import TableChartIcon from "@mui/icons-material/TableChart";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import TuneIcon from "@mui/icons-material/Tune";
 
 const Root = () => {
   const [title, setTitle] = useState("");
+  const [isActive, setIsActive] = useState("");
 
   const handleTitleChange = (e) => {
     setTitle(e.target.innerText);
-  }
+    setIsActive(e.target.innerText.toLowerCase());
+  };
 
   return (
     <>
       <header>
         <div>
-          <div><MonetizationOnIcon sx={{fontSize: 45}} /></div>
+          <div>
+            <MonetizationOnIcon sx={{ fontSize: 45 }} />
+          </div>
           <div>{title}</div>
         </div>
-        <div><AutorenewIcon sx={{fontSize:45}}/></div>
+        <div>
+          <AutorenewIcon sx={{ fontSize: 45 }} />
+        </div>
       </header>
       <nav>
-        <div>
-          <img src="#" alt="" height="30" width="30"/>
-          <NavLink to="dashboard" onClick = {handleTitleChange}>Dashboard</NavLink>
-        </div>
-        <div>
-        <img src="#" alt="" height="30" width="30"/>
-        <NavLink onClick = {handleTitleChange}>Transactions</NavLink>
-        </div>
-        <div>
-        <img src="#" alt="" height="30" width="30"/>
-        <NavLink onClick = {handleTitleChange}>Accounts</NavLink>
-        </div>
-        <div>
-        <img src="#" alt="" height="30" width="30"/>
-          <NavLink onClick = {handleTitleChange}>Reports</NavLink>
-        </div>
-        <div>
-        <img src="#" alt="" height="30" width="30"/>
-          <NavLink onClick = {handleTitleChange}>Budget</NavLink>
-        </div>
-        <div>
-        <img src="#" alt="" height="30" width="30"/>
-          <NavLink onClick = {handleTitleChange}>Settings</NavLink>
-        </div>
+        <NavLink
+          to="dashboard"
+          onClick={handleTitleChange}
+          className={isActive === "dashboard" ? "active title" : "title"}
+        >
+          <DashboardIcon sx={{ fontSize: 30 }} />
+          <span>Dashboard</span>
+        </NavLink>
+        <NavLink
+          to="transactions"
+          onClick={handleTitleChange}
+          className={isActive === "transactions" ? "active title" : "title"}
+        >
+          <SyncAltIcon sx={{ fontSize: 30 }} />
+          <span>Transactions</span>
+        </NavLink>
+        <NavLink
+          to="accounts"
+          onClick={handleTitleChange}
+          className={isActive === "accounts" ? "active title" : "title"}
+        >
+          <CreditCardIcon sx={{ fontSize: 30 }} />
+          <span>Accounts</span>
+        </NavLink>
+        <NavLink
+          to="reports"
+          onClick={handleTitleChange}
+          className={isActive === "reports" ? "active title" : "title"}
+        >
+          <TableChartIcon sx={{ fontSize: 30 }} />
+          <span>Reports</span>
+        </NavLink>
+        <NavLink
+          to="budget"
+          onClick={handleTitleChange}
+          className={isActive === "budget" ? "active title" : "title"}
+        >
+          <ShoppingBasketIcon sx={{ fontSize: 30 }} />
+          <span>Budget</span>
+        </NavLink>
+        <NavLink
+          to="settings"
+          onClick={handleTitleChange}
+          className={isActive === "settings" ? "active title" : "title"}
+        >
+          <TuneIcon sx={{ fontSize: 30 }} />
+          <span>Settings</span>
+        </NavLink>
       </nav>
-      <main><Outlet/></main>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };
