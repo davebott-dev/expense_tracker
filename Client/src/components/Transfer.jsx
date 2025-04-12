f099eb4d558a58b5d031bce7ce68f9405d45a09e
= <bottenbergdave@gmail.com>
2025-04-11 21:24:37 -0400

    styled the transfer form

--- Client/src/components/Transfer.jsx
+++ Client/src/components/Transfer.jsx
@@ -2,24 +2,31 @@ const Transfer = ({ children }) => {
     return (
         <div className="transfer">
         <form>
-            <label htmlFor="fromAccount">From Account:</label>
-            <select id="fromAccount" name="fromAccount">
-            <option value="account1">Account 1</option>
-            <option value="account2">Account 2</option>
-            <option value="account3">Account 3</option>
-            </select>
+            <div>
+                <label htmlFor="fromAccount">From Account:</label>
+                <select id="fromAccount" name="fromAccount">
+                <option value="account1">Account 1</option>
+                <option value="account2">Account 2</option>
+                <option value="account3">Account 3</option>
+                </select>
+            </div>
     
-            <label htmlFor="toAccount">To Account:</label>
-            <select id="toAccount" name="toAccount">
-            <option value="account1">Account 1</option>
-            <option value="account2">Account 2</option>
-            <option value="account3">Account 3</option>
-            </select>
+            <div>
+                <label htmlFor="toAccount">To Account:</label>
+                <select id="toAccount" name="toAccount">
+                <option value="account1">Account 1</option>
+                <option value="account2">Account 2</option>
+                <option value="account3">Account 3</option>
+                </select>
+            </div>
     
-            <label htmlFor="amount">Amount:</label>
-            <input type="number" id="amount" name="amount" required />
-    
-            <button type="submit">Transfer</button>
+            <div>
+                <div>
+                    <label htmlFor="amount">Amount:</label>
+                    <input type="number" id="amount" name="amount" required />
+                </div>
+                <button type="submit">Transfer</button>
+            </div>
         </form>
         {children}
         </div>
